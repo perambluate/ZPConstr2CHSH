af4869927d22d2b18fb88ab57e4a113ff42736d6
perambluate <dx48231@gmail.com>
2023-08-10 17:48:13 +0800

    feat: find the optimal gamma and compute the net finite rate of blind randomness with optimal params 1. In "draw_fin_rate_gamma.py", add function to find the optimal gamma and compute the finite rate.

--- blindRandomness/plotting_func/draw_fin_rate_gamma.py
+++ blindRandomness/plotting_func/draw_fin_rate_gamma.py
@@ -1,25 +1,28 @@
+"""
+    This script generate a plot of finite rate of blind randomness
+    with different and optimal gammas over number of rounds.
+"""
 from matplotlib import pyplot as plt
 from cycler import cycler
 import numpy as np
-import itertools
 from functools import partial
 from joblib import Parallel, delayed
 import math
 import os, sys
 
-### Add current directory to Python path
+### Add directory to Python path
 # (Note: this works when running the command in the dir. 'blindRandomness')
 sys.path.append('.')
-from blindRandomness.common_func.plotting_helper import *
+from common_func.plotting_helper import *
 
 ######### Plotting settings #########
-FIG_SIZE = (12, 9)    # for aspect ratio 4:3
+FIG_SIZE = (12, 9)    # aspect ratio
 DPI = 200
 SUBPLOT_PARAM = {'left': 0.085, 'right': 0.98, 'bottom': 0.11, 'top': 0.95, 'wspace': 0.28}
 
 titlesize = 30
 ticksize = 28
-legendsize = 28
+legendsize = 26
 linewidth = 3
 
 ### Set matplotlib plotting params
@@ -43,15 +46,15 @@ WIN_TOL = 1e-4                      # Tolerant error for win prob
 ### Data paths
 TOP_DIR = './'
 DATA_DIR = os.path.join(TOP_DIR, 'data/bff21_zero/w_max_77')
-OUT_DIR = os.path.join(TOP_DIR, 'figures/corrected_FER')
+OUT_DIR = os.path.join(TOP_DIR, 'figures/corrected_FER/gamma_test')
+OUTCSV_DIR = os.path.join(TOP_DIR, './data/opt_gamma')
 CLASS_INPUT_MAP = {'CHSH': '00', '1': '01', '2a': '11',
                    '2b': '01', '2b_swap': '11', '2c': '10',
                    '3a': '11', '3b': '10'}
 
-### To save file or not
-SAVE = True
-### To show figure or not
-SHOW = True
+SAVE = True         # To save figure or not
+SHOW = False        # To show figure or not
+SAVECSV = True     # To save data or not
 ### Common file names
 QUAD = 'M_12'
 EPS = f'eps_{EPSILON:.0e}'
@@ -60,7 +63,12 @@ WTOL = f'wtol_{WIN_TOL:.0e}'
 ### Varing params
 #### Number of rounds
 N_RANGE = (1e7, 1e14)
-Ns = np.geomspace(*N_RANGE, num=200)
+N_SLICE = 50
+Ns = np.geomspace(*N_RANGE, num=N_SLICE)
+
+#### Testing ratio
+GAMMA_SLICE = 50
+GAMMAs = 1/np.geomspace(10, 10000, GAMMA_SLICE)
 
 #### Param related to alpha-Renyi entropy
 BETA_SLICE = 150
@@ -71,11 +79,9 @@ NU_PRIME_SLICE = 50
 NU_PRIMEs = np.linspace(1, 0.1, num=NU_PRIME_SLICE)
 
 ### Plot with different gamma
-GAMMAs = [1e-3, 1e-2, 5e-2, 0.1, 0.5]
-CLASSES = ['CHSH','1','2c','3b']
+# GAMMAs = [1e-3, 1e-2, 5e-2, 0.1, 0.5]
+CLASSES = ['CHSH', '1', '2c']
 ZERO_TOL = 1e-9
-
-# color_iter = itertools.cycle(('blue','forestgreen','firebrick','gray'))
 
 ################################ Iter for different parameters ################################
 ### Run over all classes in CLASSES
@@ -98,9 +104,8 @@ for class_ in CLASSES:
     # print(data_mtf)
     
     fig = plt.figure(figsize=FIG_SIZE, dpi=DPI)
-    # LINES = itertools.cycle(('solid', 'dashed', 'dashdot', 'dotted', (0,(3, 1, 1, 1, 1, 1))))
     
-    for gamma in GAMMAs:
+    for gamma in [0.05, 0.01, 0.001]:
         win_prob, asym_rate, lambda_ = data_mtf[0][:3]
         c_lambda = data_mtf[0][-1]
         if class_ != 'CHSH':
@@ -109,30 +114,74 @@ for class_ in CLASSES:
 
 ################################ Compute key rate ################################
         ### Construct key rate function with fixed param (only leave n, beta tunable)
-        kr_func = partial(fin_rate_testing, gamma = gamma, asym_rate = asym_rate,
-                            lambda_ = lambda_, c_lambda = c_lambda,
-                            zero_tol = ZERO_TOL, zero_class=class_, max_p_win = max_p_win)
-        
         def opt_all(n, beta_arr = BETAs, nu_prime_arr = NU_PRIMEs):
-                return np.max(np.array([kr_func(n = n, beta = beta, nu_prime = nu_prime) \
-                                        for nu_prime in nu_prime_arr for beta in beta_arr]))
+                kr_func = partial(fin_rate_testing, gamma = gamma, asym_rate = asym_rate,
+                                    lambda_ = lambda_, c_lambda = c_lambda,
+                                    zero_tol = ZERO_TOL, zero_class=class_, max_p_win = max_p_win)
+                return np.max(np.array([kr_func(n = n, beta = beta, nu_prime = nup) \
+                                        for nup in nu_prime_arr for beta in beta_arr]))
 
-        KRs = Parallel(n_jobs=N_JOB, verbose = 0)(delayed(opt_all)(N) for N in Ns)
-        KRs = np.array(KRs)
+        FRs = Parallel(n_jobs=N_JOB, verbose = 0)(delayed(opt_all)(N) for N in Ns)
+        FRs = np.array(FRs)
 
         Cost = inp_rand_consumption(gamma, INP_DIST)
-        netKRs = KRs - Cost
-        netKRs[netKRs < 0] = 0
+        netFRs = FRs - Cost
+        netFRs[netFRs < 0] = 0
 
         label = r'$\displaystyle \gamma={:.1g}$'.format(gamma)
-        plt.plot(Ns, netKRs, label = label)
+        plt.plot(Ns, netFRs, label = label)
 
+    def opt_with_testing(n, beta_arr = BETAs, nup_arr = NU_PRIMEs, gam_arr = GAMMAs):
+        kr_func = partial(fin_rate_testing, asym_rate = asym_rate,
+                            lambda_ = lambda_, c_lambda = c_lambda,
+                            zero_tol = ZERO_TOL, zero_class=class_, max_p_win = max_p_win)
+        gen_rand = np.array([[kr_func(n = n, beta = beta, nu_prime = nup, gamma = gamma) \
+                                for nup in nup_arr for beta in beta_arr] for gamma in gam_arr])
+        cost = np.array([inp_rand_consumption(gamma, INP_DIST) for gamma in GAMMAs])
+        net_rand = (gen_rand.T - cost).T
+        # print(net_rand.shape)
+        max_id =  np.argmax(net_rand) + 1
+        # print(max_id)
+        # print(math.ceil(max_id / (NU_PRIME_SLICE*BETA_SLICE)))
+        max_gamma = GAMMAs[math.ceil(max_id / (NU_PRIME_SLICE*BETA_SLICE))-1]
+        max_id = max_id % (NU_PRIME_SLICE*BETA_SLICE)
+        max_beta = BETAs[max_id % BETA_SLICE]
+        max_id = math.ceil(max_id / BETA_SLICE)
+        max_nup = NU_PRIMEs[max_id-1]
+        opt_fin_rate = kr_func(n = n, beta = max_beta, nu_prime = max_nup, gamma = max_gamma) -\
+                         inp_rand_consumption(max_gamma, INP_DIST)
+        return opt_fin_rate, max_gamma
+    
+    opt_gamma = Parallel(n_jobs=N_JOB, verbose = 0)(delayed(opt_with_testing)(N) for N in Ns)
+    opt_gamma = list(zip(*opt_gamma))
+    optFRs = np.array(opt_gamma[0])
+    optGAMMAs = np.array(opt_gamma[1])
+
+    plt.scatter(Ns, optFRs, label = r'optimal $\displaystyle \gamma$')
+    nonzero = N_SLICE - len(optFRs[optFRs>=1e-6])
+    for i in range(nonzero, N_SLICE):
+        if (i-nonzero) % 10 == 0 or i == nonzero+4:
+            txt = f'{optGAMMAs[i]:.1e}'
+            plt.annotate(txt, (Ns[i], optFRs[i]+0.01), fontsize=24, horizontalalignment='right')
+
+    ### Save optimal gamma to num of rounds to CSV file
+    if SAVECSV:
+        data2save = np.column_stack((Ns[nonzero:N_SLICE],
+                                     optFRs[nonzero:N_SLICE],
+                                     optGAMMAs[nonzero:N_SLICE]))
+        HEADER = 'num_of_rounds\trate\tgamma'
+        WEXP = f'w_{win_prob*10000:.0f}'.rstrip('0')
+        OUTFILE = f'opt_gam-{CLS}-{WEXP}-{EPS}-{WTOL}-{ZTOL}-{QUAD}.csv'
+        out_path = os.path.join(OUTCSV_DIR, OUTFILE)
+        np.savetxt(out_path, data2save, fmt='%.5g', delimiter=',', header=HEADER)
+
+    ### Set x-/y- labels and other plotting stuff
     YLABEL = r'$\displaystyle r$'+' (bit per round)'
     XLABEL = r'$\displaystyle n$'+' (number of rounds)'
     plt.ylabel(YLABEL)
     plt.xlabel(XLABEL)
     plt.xscale("log")
-    plt.legend(prop={"weight":"bold"})
+    plt.legend(prop={"weight":"bold"}, loc='lower right')
 
     ### Apply the graphic settings
     plt.subplots_adjust(**SUBPLOT_PARAM)
@@ -140,11 +189,11 @@ for class_ in CLASSES:
     ### Save file
     if SAVE:
         ### General File Name Settings
-        COM = 'diff_test_ratio'
+        COM = 'opt_test_ratio'
         WEXP = 'QBOUND'
-        TAIL = ''
+        TAIL = '1'
         FORMAT = 'png'
-        OUT_NAME = f'{COM}-{CLS}-{WEXP}-{EPS}-{WTOL}-{QUAD}'
+        OUT_NAME = f'{COM}-{CLS}-{WEXP}-{EPS}-{WTOL}-{ZTOL}-{QUAD}'
         if TAIL:
             OUT_NAME += f'-{TAIL}'
         out_path = os.path.join(OUT_DIR, f'{OUT_NAME}.{FORMAT}')
